7b66e744920880afcdabee8d0285416c791820b4
Goutham Rangarajan <grangarajan@ana-data.com>
2022-09-21 14:24:16 -0400

    cancellatio token usage poc

---
+++ cancellation-token-101/Controllers/WeatherForecastController.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace cancellation_token_101.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public async Task<IEnumerable<WeatherForecast>?> Get(CancellationToken token)
+    {
+        try{
+            //RG the below can be anything e.g fetching records from db, fetching records from another api
+            //almost all objects/library used in this scenario accepts Cancellation token 
+            // e.g httpclient/ dapper
+            await Task.Delay(4000,token);
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        catch(Exception e){
+            _logger.LogError(e,"Exception occured in GetWeatherForecast");
+            return null;
+        }
+    }
+    [HttpPost(Name="PostWeatherForeCast")]
+    public async Task<IEnumerable<WeatherForecast>?> Post([FromBody]PostRequest request,CancellationToken token)
+    {
+        try{
+            await Task.Delay(4000,token);
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        catch(Exception e){
+            _logger.LogError(e,$"Exception occured in PostWeatherForecast {request.Id}");
+            return null;
+        }
+    }
+}
+
+public class PostRequest
+{
+    public int Id {get;set;}
+}
---
+++ cancellation-token-101/README.md
@@ -0,0 +1,31 @@
+### Cancellation Token 101
+
+#### very useful to cancel a process/ resource access
+
+##### sample to cancel a data generation in Weatherforecast
+
+```C#
+public async Task<IEnumerable<WeatherForecast>?> Get(CancellationToken token)
+{
+    try{
+        //RG the below can be anything e.g fetching records from db, fetching records from another api
+        //almost all objects/library used in this scenario accepts Cancellation token
+        // e.g httpclient/ dapper
+        await Task.Delay(4000,token);
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+    catch(Exception e){
+        _logger.LogError(e,"Exception occured in GetWeatherForecast");
+        return null;
+    }
+}
+```
+
+- To test it , hit the get url in browser https://localhost:7257/weatherforecast and close the browser tab within 4 seconds.
+- We can see the exception getting logged
